eb964639d9780b1a925a2fb25685304a4224976c
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-05-23 10:54:57 +0200

    update 23_05_2022

--- script/menu.js
+++ script/menu.js
@@ -1,4 +1,4 @@
-
+/*
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -6,4 +6,22 @@ function myFunction() {
 } else {
     x.style.display = "block";
 }
+}*/
+
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    sidenav.classList.add("active");
 }
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    sidenav.classList.remove("active");
+}
+
