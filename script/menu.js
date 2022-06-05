70cadc0dc92ce85f054ca45437d659313a3839df
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-06-05 15:54:11 +0200

    update 05_06_2022

--- script/menu.js
+++ script/menu.js
@@ -1,28 +1,8 @@
-/*
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
-    x.style.display = "block";
-}
-}*/
+var toggle = document.getElementById("nav-toggle");
+var navContainer = document.getElementById("nav-container");
+console.log(toggle);
 
-var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
-
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
-
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    sidenav.classList.add("active");
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    sidenav.classList.remove("active");
-    element.style.display = 'none';
-}
-
+toggle.addEventListener('click', function() {
+    console.log('i did a thing');
+    navContainer.classList.toggle('active');
+});
